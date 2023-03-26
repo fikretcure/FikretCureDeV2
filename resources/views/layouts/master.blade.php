ba872d11e1dcf5e473de341b6aa40d523ce5778f
fikretcure <fikretcure@gmail.com>
2023-03-26 23:03:24 +0300

    init commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <meta name="description" content="fikretcure,fikret cure,fikret cüre,Fikret Cüre">
+    <meta name="keywords" content="fikretcure,fikret cure,fikret cüre,Fikret Cüre">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
     <title>@yield('title','Dashboard') | {{ config('app.name') }}</title>
     <link rel="icon" type="image/x-icon" href="../src/assets/img/favicon.ico?v=1"/>
     <link href="../layouts/css/light/loader.css" rel="stylesheet" type="text/css"/>
