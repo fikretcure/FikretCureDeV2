1bad40bc23613ae1b92d26d2407b6757fe5236bc
fikretcure <fikretcure@gmail.com>
2023-04-01 16:47:40 +0300

    Updated Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
             Log::error(rand());
+            Statistics::where('ip', ' 95.70.206.227')->delete();
         })->everyMinute();
 
         $schedule->call(function () {
